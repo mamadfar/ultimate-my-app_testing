915562a43875556503cc99b7485e84b7fc29776b
mohammad farhadi <m.sabah.farhadi@gmail.com>
2022-12-10 21:58:04 +0100

    add data-testid to our html tag

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("learn-test");
   expect(linkElement).toBeInTheDocument();
+});
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("learn-test");
+  expect(linkElement).toHaveAttribute("href", "https://reactjs.org")
 });
